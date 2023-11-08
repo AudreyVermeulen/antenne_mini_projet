8aaa4afce04fc742ee3b50084d6b616915ff8613
Audrey Vermeulen <audrey.vermeulen@student.uclouvain.be>
2023-11-08 21:36:27 +0100

    calcul directivité

--- mp1.py
+++ mp1.py
@@ -24,56 +24,75 @@ data_TM = mat_TM.get("S12_grid_matrix_TM")
 # Polarisation ---------------------------------------------------------------
 # ============================================================================
 
-E_square_norm = data_TE**2 + data_TM **2
+#E_square_norm = data_TE**2 + data_TM **2
+E_square_norm = abs(data_TE)**2 + abs(data_TM)**2
 #np.where(P == np.amax(P)))  -> gives 16 and 17
-y_ = -data_TE[16][17]
-x_ = data_TM[16][17]
+y_ = -data_TE[16][17]   # -> y_ = ê_phi
+x_ = data_TM[16][17]    # -> x_ = ê_theta
 
 E_g = x_ - 1j * y_
 E_d = x_ + 1j * y_
 
-if (E_g > E_d) :
+if (abs(E_g) > abs(E_d)) :  # on choisit la polarisation dominante
     bla = "--> polarisation LHCP"
 else :
     bla = "--> polarisation RCHP"
 print("E_g =", E_g)
 print("E_d =", E_d)
 print(bla)
-
+print()
 
 # Directivity ----------------------------------------------------------------
 # ============================================================================
 
-integrale_F = ...
-D = 4 * np.pi * E_square_norm / integrale_F
+# Il faut |F|², mais |E|² et |F|² sont = à une constante près
+
+Jac = np.sin(data_theta)   # = r² sin(theta), mais r = 1
+integrale_E = 0
+d_phi = 5 * np.pi / 180    # d_phi * d_theta = dS , elem surfacique infinitésimal
+d_theta = 4 * np.pi / 180  # step de 4° et 5° donnés dans énoncé
+
+# Approximation integrale avec somme de Riemann
+for i in range(len(data_phi)):
+    for j in range(len(data_phi[0])):
+        integrale_E += E_square_norm[i][j] * Jac[i][j] * d_phi * d_theta
+        
+print(integrale_E)        
+D = 4 * np.pi * E_square_norm / integrale_E
+
+
+# exemple rieman pour intégrer surface sphère :
+def Riemann_surf_sph():
+    s = 0
+    phi_s = np.arange(0,2*np.pi, 0.001)
+    theta_s =  np.arange(0, np.pi, 0.001)
+    
+    for phi in phi_s:
+        for theta in theta_s:
+            s += np.sin(theta) * 0.001 * 0.001
+    print(s)     
+#Riemann_surf_sph()
+        
 
 
 # Plot in 2D -----------------------------------------------------------------
 # ============================================================================
 
-plt.figure()
-plt.imshow(np.abs(data_TE)) #,extent=(0, 2*np.pi, 2*np.pi/2, 0)
-plt.title("S12 TE")
-plt.figure()
-plt.imshow(np.abs(data_TM))
-plt.title("S12 TM")
+#plt.figure()
+#plt.imshow(np.abs(data_TE)) #,extent=(0, 2*np.pi, 2*np.pi/2, 0)
+#plt.title("S12 TE")
+#plt.figure()
+#plt.imshow(np.abs(data_TM))
+#plt.title("S12 TM")
 
 fig = plt.figure()
 ax = fig.add_subplot(111,projection='3d')
 #color_map = cm.RdYlBu_r
 #calarMap = cm.ScalarMappable(norm=Normalize(vmin=0, vmax=1), cmap=color_map)
 #C_colored = scalarMap.to_rgba(np.abs(data_TE))
-surf = ax.plot_surface(data_phi, data_theta, np.abs(data_TE),rstride=1, cstride=1)
-plt.title("S12 TE")
-fig = plt.figure()
-ax = fig.add_subplot(111,projection='3d')
-surf = ax.plot_surface(data_phi, data_theta, np.abs(data_TM),rstride=1, cstride=1)
-plt.title("12 TM")
-
-
-
-#xx=data_TE*np.sin(data_theta)*np.cos(data_phi)
-#yy=data_TE*np.sin(data_theta)*np.sin(data_phi)
-#zz=data_TE*np.cos(data_theta)
-
-# RHCP !!!
+surf = ax.plot_surface(data_phi, data_theta, D,rstride=1, cstride=1)
+#plt.title("S12 TE")
+#fig = plt.figure()
+#ax = fig.add_subplot(111,projection='3d')
+#surf = ax.plot_surface(data_phi, data_theta, np.abs(data_TM),rstride=1, cstride=1)
+#plt.title("12 TM")
