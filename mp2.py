72fd6e7dacc12030f1c416c4fa12499a17ec162c
Audrey Vermeulen <audrey.vermeulen@student.uclouvain.be>
2023-11-10 15:45:26 +0100

    directivité de chaque antenne

--- mp2.py
+++ mp2.py
@@ -1,4 +1,3 @@
-import scipy.io
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -31,7 +30,7 @@ chemin_T = 'ton chemin pour accéder aux fichiers renvoyés par le code Matlab'
 
 # Setup le bon chemin pour pas avoir de problèmes avec git
 while (True):
-    chemin = input("Chemin de A ou T?")
+    chemin = input("Chemin de A ou T ?  ")
     if (chemin == "A" or chemin == "a"):
         chemin = chemin_A
         print()
@@ -110,6 +109,61 @@ for i in range(1,params.get("N")+1):
 print()    
 
 
+# Calcul Directivité ---------------------------------------------------------
+# ============================================================================
+
+
+"""
+Jsp trop comment calculé la directivité.
+- Eske le F d'une antenne correspond à son F quand l'antenne est seule ? Ou 
+  bien le F donné prend déjà en compte l'impact des autres antennes ?
+- Pour obtenir le F total, il faut faire le produit de tous les F ? Ou bien la 
+  somme ?
+- Pour calculer F_tot, esk'il faut d'abord sommer/multiplier tous les F et puis
+  prendre la abs? Ou bien l'inverse ?
+"""
+
+F = real_F + imag_F * 1j
+F_square_norm = abs(F)**2
+
+def Riemann(f) :
+    """
+    Approximation de l'intégrale de f sur la surface d'une sphère 
+    de rayon unitaire.
+    """
+    Jac = np.sin(theta)   
+    integrale = 0              
+    d_phi = (360/80) * np.pi / 180    
+    d_theta = (180/60) * np.pi / 180  
+
+    for i in range(60):       # theta
+        for j in range(80):   # phi
+            integrale += f[j + i*80] * Jac[i] * d_phi * d_theta
+    
+    return integrale
+  
+    
+# Directivité pour chaque patterne :
+    
+mesh_phi, mesh_theta = np.meshgrid(phi, theta)  
+  
+for i in range(0,params.get("N"),3): # 3 diagrammes par figure
+    fig = plt.figure()
+    for j in range(3):
+    
+        D = 4 * np.pi * F_square_norm[i+j] / Riemann(F_square_norm[i+j])
+        D = D.reshape(60,80)
+        
+        u = D * np.sin(mesh_theta) * np.cos(mesh_phi)
+        v = D * np.sin(mesh_theta) * np.sin(mesh_phi)
+        w = D * np.cos(mesh_theta)
+        
+        sub = "13"+str(j+1)
+        ax = fig.add_subplot(int(sub),projection='3d')
+        surf = ax.plot_surface(u, v, w,rstride=1, cstride=1)
+    plt.title("Directivity versus u-v plane : pattern "+str(i+1)+" - "+str(i+3)) 
+
+
 # Affichage console ----------------------------------------------------------
 # ============================================================================
 
@@ -123,7 +177,7 @@ print()
 
 if (count_err > 0):
     print("-------- WARNIG --------")
-    print(len(idx_err), "Erreurs supérieures à 1%.")
+    print(len(idx_err), "Erreur(s) supérieure(s) à 1%.")
     for i in idx_err :
         print("Antenne",i,":",errors[i])
     print()
